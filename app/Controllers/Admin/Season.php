d394fa63262d3bc403766d1f8a501f85056624bb
tanguy <email@email.fr>
2026-02-04 14:50:57 +0100

    feat : création d'une saison

--- app/Controllers/Admin/Season.php
+++ app/Controllers/Admin/Season.php
@@ -3,10 +3,17 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\SeasonModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Season extends AdminController
 {
+    protected $sm;
+
+    public function __construct(){
+        $this->sm = new SeasonModel();
+    }
+
     public function index()
     {
         $data = [
@@ -14,5 +21,26 @@ class Season extends AdminController
         ];
         $this->addBreadcrumb('Saison','');
         return $this->render('admin/season',$data);
+    }
+
+    public function insertSeason(){
+        try {
+            $dataSeason = [
+                'name' => $this->request->getPost('name'),
+                'start_date' => $this->request->getPost('start_date'),
+                'end_date' => $this->request->getPost('end_date'),
+            ];
+            if($this->sm->insert($dataSeason)){
+                $this->success('Saison créée avec succès');
+            } else {
+                foreach ($this->sm->errors() as $error) {
+                    $this->error($error);
+                }
+            }
+            return $this->redirect('admin/season');
+        } catch (\Exception $e) {
+            $this->error = $e->getMessage();
+            return redirect()->back()->withInput();
+        }
     }
 }
--- app/Views/admin/season.php
+++ app/Views/admin/season.php
@@ -5,7 +5,38 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            création saison
+            <!-- START : ZONE CREATION -->
+            <div class="card">
+                <?= form_open('/admin/season/insert') ?>
+                <div class="card-header">
+                    <span class="card-title h5"> Création d'une nouvelle saison</span>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                       <div class="col">
+                           <label class="form-label" for="name">Nom de la saison <span class="text-danger">*</span></label>
+                           <input class="form-control" type="text" name="name" id="name" value="<?=old('name')?>" required>
+                       </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label class="form-label" for="start_date">Date de début de saison <span class="text-danger">*</span></label>
+                            <input class="form-control" type="date" name="start_date" id="start_date" value="<?=old('start_date')?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label class="form-label" for="end_date">Date de fin de saison <span class="text-danger">*</span></label>
+                            <input class="form-control" type="date" name="end_date" id="end_date" value="<?=old('start_date')?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-end">
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-plus"></i> Créer la saison</button>
+                </div>
+                <?= form_close() ?>
+            </div>
+            <!-- END : ZONE CREATION -->
         </div>
         <div class="col-md-8">
             index saison
