129d9930415c272fff69b92134b575e2aef3420c
tanguy <email@email.fr>
2026-09-07 23:05:33 +0200

    feat : gestion suppression des contacts Member et Club

--- app/Views/admin/club/form.php
+++ app/Views/admin/club/form.php
@@ -104,12 +104,12 @@
                                     <div class="col-md-6 mb-3">
                                         <div class="row">
                                             <div class="col-6">
-                                                <label class="form-label" for="phone_number">Numéro de telephone</label>
+                                                <label class="form-label" for="phone_number<?=$nbContacts?>">Numéro de telephone</label>
                                                 <input class="form-control" type="text" id="phone_number<?=$nbContacts?>" name="contacts[<?=$nbContacts?>][phone_number]" value="<?= old('contact.'
                                                         .$nbContacts.'.phone_number',esc($contact['phone_number']));?>">
                                             </div>
                                             <div class="col-6">
-                                                <label class="form-label" for="mail">Adresse e-mail</label>
+                                                <label class="form-label" for="mail<?=$nbContacts?>">Adresse e-mail</label>
                                                 <input class="form-control" type="text" id="mail<?=$nbContacts?>" name="contacts[<?=$nbContacts?>][mail]" value="<?= old('contact.'.$nbContacts.'.mail',esc($contact['mail']));?>">
                                             </div>
                                         </div>
@@ -117,21 +117,22 @@
                                     <div class="col-md-6 mb-3">
                                         <div class="row">
                                             <div class="col">
-                                                <label class="form-label" for="details">Détails du contact <span class="fw-lighter fst-italic">(optionnel, max. 255 caractères)</span></label>
+                                                <label class="form-label" for="details<?=$nbContacts?>">Détails du contact <span class="fw-lighter fst-italic">(optionnel, max. 255 caractères)</span></label>
                                                 <textarea class="form-control" name="contacts[<?=$nbContacts?>][details]}" id="details<?=$nbContacts?>" rows="2" ><?= old('contact.' .$nbContacts.'.details',esc($contact['details']));?></textarea>
                                             </div>
                                             <div class="col-auto d-flex align-items-center">
-                                                <span class="fs-4" id="delete-contact-"><i class="fas fa-trash-alt text-danger delete-contact-button"></i></span>
+                                                <span class="fs-4" id="delete-contact-<?=$nbContacts?>"><i class="fas fa-trash-alt text-danger delete-contact-button"></i></span>
                                             </div>
                                         </div>
                                     </div>
-                                    <input type="hidden" id="contact-id-input<?=$nbContacts?>" name="contacts[<?=$nbContacts?>][id]" value="<?= old('contact.'.$nbContacts.'.id',esc($contact['id']));
+                                    <input type="hidden" id="contact-id-input<?=$nbContacts?>" name="contact-id-input[]" value="<?= old('contact.'.$nbContacts.'.id',esc($contact['id']));
                                     ?>">
                                 </div>
-                                <!-- START : ZONE HTML POUR STOCKER LES ID DES CONTACTS SUPPRIMES -->
+                                <!-- START : INPUT HTML POUR STOCKER LES ID DES CONTACTS SUPPRIMES -->
                                 <div id="zone-removed-contacts">
 
                                 </div>
+                                <!-- END : INPUT HTML POUR STOCKER LES ID DES CONTACTS SUPPRIMES -->
                             <?php }
                         } ?>
                     </div>
@@ -152,8 +153,6 @@
                                                 </a>
                                             </div>
                                         </div>
-
-
                                     </div>
                                     <div class="card-body">
 
@@ -333,6 +332,8 @@ $(document).ready(function () {
                         </div>
                     </div>
                 </div>
+                <div id="zone-removed-contacts${nbContacts}">
+                </div>
             </div>
         `;
         $('#zone-contact').append(row);
@@ -340,10 +341,9 @@ $(document).ready(function () {
 
     //Gestion de la suppression d'un contact
     $('#zone-contact').on('click', '.delete-contact-button' ,function(){
-        nbContacts --;
         let rowContact = $(this).closest('.row-contact');
-        let idRemovedContact = rowContact.find('#contact-id-input').val();
-        console.log(idRemovedContact);
+        let idRemovedContact = rowContact.find('#contact-id-input'+nbContacts).val();
+        nbContacts --;
         rowContact.remove();
         let inputRemovedContacts = `
         <input type="hidden" name="removed-contacts[]" value="${idRemovedContact}">
@@ -426,7 +426,7 @@ $(document).ready(function () {
         color: black;
     }
 
-    .card-team:hover,.card-gym:hover, .delete-gym-button:hover{
+    .card-team:hover,.card-gym:hover, .delete-gym-button:hover, .delete-contact-button:hover{
         scale:1.05;
         cursor: pointer;
     }
